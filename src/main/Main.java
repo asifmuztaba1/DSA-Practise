3dc5a0d32a0195b8ba300de3f4f9b7250285b2b6
asifmuztaba1 <amuztaba18@gmail.com>
2023-08-20 21:20:47 +0600

    output with time complexity info

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,28 @@
 package main;
+
 import datastructure.DSAArrays;
+import complexity.ComplexityAnalyzer;
 
 import java.util.Arrays;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        DSAArrays arrays=new DSAArrays(new int[]{0, 1, 2, 3, 4});
-        System.out.print(Arrays.toString(arrays.rotateArray(1)));
+        DSAArrays arrays = new DSAArrays(new int[]{0, 1, 2, 3, 4});
+
+        // Create an instance of ComplexityAnalyzer
+        ComplexityAnalyzer<int[]> analyzer = new ComplexityAnalyzer<>();
+
+        // Define an operation that calls the rotateArray method
+        ComplexityAnalyzer.Operation<int[]> rotateOperation = input -> {
+            int position = 1; // Specify the rotation position
+            arrays.rotateArray(position);
+        };
+
+        // Analyze the time and space complexity of the rotate operation
+        analyzer.analyzeOperation(rotateOperation, arrays.getElement());
+        long elapsedTime = analyzer.getElapsedTime();
+
+        // Use the static method to get the time complexity
+        System.out.println("Time Complexity: " + ComplexityAnalyzer.getTimeComplexity(elapsedTime, arrays.getElement().length));
     }
 }
