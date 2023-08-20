fbb9e98d5f32d9568201924701e8acba4ebdd28a
asifmuztaba1 <amuztaba18@gmail.com>
2023-08-20 20:07:33 +0600

    Initialized Array Class, and created method rotateArray

---
+++ src/datastructure/DSAArrays.java
@@ -0,0 +1,22 @@
+package datastructure;
+
+public class DSAArrays {
+    private int[] element;
+    public DSAArrays(int[] element) {
+        this.element = element;
+    }
+
+    public int[] rotateArray(int postion){
+        int length = this.element.length;
+        int counter=1;
+        while (counter<=postion){
+            int last=this.element[0];
+            for (int i=0;i<length-1;i++){
+                this.element[i]=this.element[i+1];
+            }
+            this.element[length-1]=last;
+            counter++;
+        }
+        return this.element;
+    }
+}
